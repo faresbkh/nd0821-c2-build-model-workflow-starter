771f114dcc8b3ad4bbce4154af26bacd3776291c
Fares El Kahla <benkahlafares@gmail.com>
2022-02-27 19:07:51 +0100

    eda and preprocessing

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
-
-import mlflow
-import tempfile
 import os
-import wandb
+import tempfile
+
 import hydra
+import mlflow
+import wandb
 from omegaconf import DictConfig
 
 _steps = [
@@ -16,12 +16,12 @@ _steps = [
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
-#    "test_regression_model"
+    #    "test_regression_model"
 ]
 
 
 # This automatically reads in the configuration
-@hydra.main(config_name='config')
+@hydra.main(config_name="config")
 def go(config: DictConfig):
 
     # Setup the wandb experiment. All runs will be grouped under this name
@@ -29,7 +29,7 @@ def go(config: DictConfig):
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
 
     # Steps to execute
-    steps_par = config['main']['steps']
+    steps_par = config["main"]["steps"]
     active_steps = steps_par.split(",") if steps_par != "all" else _steps
 
     # Move to a temporary directory
@@ -44,15 +44,23 @@ def go(config: DictConfig):
                     "sample": config["etl"]["sample"],
                     "artifact_name": "sample.csv",
                     "artifact_type": "raw_data",
-                    "artifact_description": "Raw file as downloaded"
+                    "artifact_description": "Raw file as downloaded",
                 },
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                "main",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_sample",
+                    "output_description": "Data with outliers and null values removed",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
 
         if "data_check" in active_steps:
             ##################
---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+"""
+import argparse
+import logging
+import os
+
+import pandas as pd
+import wandb
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    ######################
+    # YOUR CODE HERE     #
+    ######################
+    dataframe = pd.read_csv(artifact_local_path, index_col="id")
+    minimum_price = args.min_price
+    maximum_price = args.max_price
+    idx = dataframe["price"].between(minimum_price, maximum_price)
+    dataframe = dataframe[idx].copy()
+    dataframe["last_review"] = pd.to_datetime(dataframe["last_review"])
+
+    dataframe.to_csv(args.output_artifact)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
+    artifact.wait()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+    parser.add_argument("--input_artifact", type=str, help="Input artifact name", required=True)
+
+    parser.add_argument("--output_artifact", type=str, help="Output artifact name", required=True)
+
+    parser.add_argument("--output_type", type=str, help="Output artifact type", required=True)
+
+    parser.add_argument(
+        "--output_description", type=str, help="Output artifact description", required=True
+    )
+
+    parser.add_argument("--min_price", type=int, help="Minimum price", required=True)
+
+    parser.add_argument("--max_price", type=int, help="Maximum price", required=True)
+
+    args = parser.parse_args()
+
+    go(args)
